2b1f7ae7b6141a9d617bebea11a929adc42c4cf1
antcosic <antcosic@foi.hr>
2016-12-06 18:01:57 +0100

    simpy added

--- Python_kalkulator_klasice_logike_sudova/Python_kalkulator_klasice_logike_sudova.py
+++ Python_kalkulator_klasice_logike_sudova/Python_kalkulator_klasice_logike_sudova.py
@@ -1,4 +1,5 @@
 import pyparsing as pp
+from sympy import Symbol, cos
 
 operator = pp.Regex(">=|<=|!=|>|<|=").setName("operator")
 number = pp.Regex(r"[+-]?\d+(:?\.\d*)?(:?[eE][+-]?\d+)?")
@@ -11,5 +12,9 @@ expr = pp.operatorPrecedence(condition,[
                             ("AND", 2, pp.opAssoc.LEFT, ),
                             ("OR", 2, pp.opAssoc.LEFT, ),
                             ])
+
+x = Symbol('x')
+e = 1/cos(x)
+print e.series(x, 0, 10)
 
 print expr.parseString("NOT x > 7 AND x < 8 OR x = 4")
